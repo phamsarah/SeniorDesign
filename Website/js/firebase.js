d1d106762ed2b807a1c541790e3cf4047de39889
Austin Newkirk <austinjaynew@gmail.com>
2021-02-10 17:44:26 -0600

    Some change; addition of firebase.js
    Some change; addition of firebase.js

---
+++ Website/js/firebase.js
@@ -0,0 +1,49 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyABrXbvSkA_LqT0zKSTJ3_JHqNcxgyuxsQ",
+    authDomain: "music-matters-229420.firebaseapp.com",
+    databaseURL: "https://music-matters-229420.firebaseio.com",
+    projectId: "music-matters-229420",
+    storageBucket: "music-matters-229420.appspot.com",
+    messagingSenderId: "939526187420",
+    appId: "1:939526187420:web:9bfd8eb38ec627a575c059"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  function signIn() {
+		var email = document.getElementById('email').value;
+		var password = document.getElementById('password').value;
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+		.then((userCredentials) => {
+			var user = userCredentials.user;
+			alert("Signed in");
+		  })
+		  .catch((error) => {
+			alert("Incorrect");
+		  });
+
+  	}
+
+	/*  firebase.auth().onAuthStateChanged(function(user){
+  
+		if(user){
+		 
+		 var email = user.email;
+		 alert("Active User " + email);
+		 
+		 //Take user to a different or home page
+	  
+		 //is signed in
+		 
+		}else{
+		 
+		 alert("No Active User");
+		 //no user is signed in
+		}
+
+	});
+ */
--- Website/js/main.js
+++ Website/js/main.js
@@ -1,15 +1,26 @@
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+	  // User is signed in.
+	} else {
+	  // No user is signed in.
+	}
+  });
+
 function clicked() {
-	var user = document.getElementById('username');
-	var pass = document.getElementById('password');
-	
-	var validuser = "Admin";
-	var validpass = "Password";
-	
-	if(user.value == validuser && pass.value == validpass) {
-		window.alert("You are logged in as " + user.value);
-		window.location.href="pages/Homepage.html";
-	}
-	else  {
-		window.alert("Incorrect username or password");
-	}
+	var user = document.getElementById('username').value;
+	var pass = document.getElementById('password').value;
 }
+
+firebase.auth().signInWithEmailAndPassword(user, pass)
+  .then((userCredential) => {
+    // Signed in
+	var user = userCredential.user;
+	window.alert("You are logged in as " + user.value);
+		window.location.href="pages/Homepage.html";
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+	var errorMessage = error.message;
+	window.alert("Incorrect username or password");
+  });
