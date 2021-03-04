346d38cb6d0828e455e595955ee2bd3abc2d8866
Austin Newkirk <austinjaynew@gmail.com>
2021-03-04 14:37:31 -0600

    Update main.js

--- Website/jQuery_calendar/lib/main.js
+++ Website/jQuery_calendar/lib/main.js
@@ -19,7 +19,9 @@ var FullCalendar = (function (exports) {
     LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
     OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
     PERFORMANCE OF THIS SOFTWARE.
-    ***************************************************************************** */
+    ***************************************************************************** 
+    https://fullcalendar.io/docs#toc
+    */
     /* global Reflect, Promise */
 
     var extendStatics = function(d, b) {
